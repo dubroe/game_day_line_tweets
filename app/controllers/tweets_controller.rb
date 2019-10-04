757fdd4eae62fd887d8806fd02adcecc663e52d7
dubroe <elan.dubrofsky@gmail.com>
2019-10-04 10:55:47 -0400

    Display tweets on root page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,12 @@
 class TweetsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def index
+    @team = Team.find_by(abbr: params[:team])
+    tweets_relation = @team&.tweets || Tweet
+    @tweets = tweets_relation.last(50)
+  end
+
   def create
     handle = params[:text].scan(/RT @(.*?):/).flatten.first
     beat_writer = BeatWriter.find_by(handle: handle)
