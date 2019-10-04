541b0ba5c9ffc98eb51eebd2c194fbb10e8718af
dubroe <elan.dubrofsky@gmail.com>
2019-10-04 13:03:27 -0400

    Add form to select team in UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def team_options(current_team)
+    team_list = Team.all.map { |team| [team.name, team.abbr] }
+    team_list.unshift(['All Teams', 'ALL'])
+    options_for_select(team_list, current_team&.abbr)
+  end
 end
