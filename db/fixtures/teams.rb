4c8285e5e7527af73156b56e26f4d02ca6f03728
dubroe <elan.dubrofsky@gmail.com>
2019-10-07 12:26:09 -0400

    Add brooksbratten

--- db/fixtures/teams.rb
+++ db/fixtures/teams.rb
@@ -116,4 +116,5 @@ BeatWriter.seed(
   { id: 222, handle: 'Jack_A_Harris', team_abbr: 'LAK' },
   { id: 223, handle: 'engelheather', team_abbr: 'BUF' },
   { id: 224, handle: 'DallasStars', team_abbr: 'DAL' },
+  { id: 225, handle: 'brooksbratten', team_abbr: 'NSH' },
 )
