ee5e9ed8721ae999bdbe950433154d11833a1130
dubroe <elan.dubrofsky@gmail.com>
2019-10-14 21:08:30 -0400

    Add Bob_Stauffer

--- db/fixtures/teams.rb
+++ db/fixtures/teams.rb
@@ -128,4 +128,5 @@ BeatWriter.seed(
   { id: 234, handle: 'JacketsInsider', team_abbr: 'CBJ' },
   { id: 235, handle: 'MikeDeFabo', team_abbr: 'PIT' },
   { id: 236, handle: 'LLysowski', team_abbr: 'BUF' },
+  { id: 237, handle: 'Bob_Stauffer', team_abbr: 'EDM' },
 )
