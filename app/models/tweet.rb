515ef4fdbe1358d8ddb274e5ffca808c3ccfee9b
dubroe <elan.dubrofsky@gmail.com>
2019-11-24 15:17:56 -0500

    Only show tweets when they are loaded

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,8 @@
 
 class Tweet < ApplicationRecord
   belongs_to :beat_writer
+
+  def initially_hidden_embed_code
+    tweet_embed_code.gsub('blockquote', 'blockquote style="display:none;"')
+  end
 end
